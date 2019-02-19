64f3b4a100c624386bafec22959758c599a157c7
Daniel Wong <drojfjord@gmail.com>
2019-02-19 17:16:38 +1100

    operate within the subfolder 'install_data', rather than in the root directory, to keep the folder clean.

--- bootstrap/higu_win_installer_32/install.bat
+++ bootstrap/higu_win_installer_32/install.bat
@@ -1,9 +1,10 @@
 @echo off
+cd install_data
 IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z
 aria2c --allow-overwrite=true --auto-file-renaming=false https://raw.githubusercontent.com/07th-mod/python-patcher/master/scriptDownloadList.txt
-aria2c --allow-overwrite=true --auto-file-renaming=false --input-file=scriptDownloadList.txt --dir=scripts
-del scriptDownloadList.txt
-"python/python.exe" scripts/main.py
+aria2c --allow-overwrite=true --auto-file-renaming=false --input-file=scriptDownloadList.txt
+"python/python.exe" main.py
+
 echo ----------------------------------------------------------- 
 echo ------------ Batch file has finished executing ------------
 echo ------------ Press any key to close this window -----------  
