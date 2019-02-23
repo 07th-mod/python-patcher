eaf83d3951d9279f02d47b2685818d2c1b4e3b35
Tellow Krinkle <tellowkrinkle@gmail.com>
2019-02-22 22:34:02 -0600

    use if __name__ == "__main__"

--- main.py
+++ main.py
@@ -24,42 +24,44 @@ dirname = os.path.dirname(sys.argv[0])
 if dirname.strip():
 	os.chdir(dirname)
 
-#redirect stdout to both a file and console
-#TODO: on MAC using a .app file, not sure if this logfile will be writeable
-#      could do a try-catch, and then only begin logging once the game path has been set?
-sys.stdout = logger.Logger("logfile.log")
-logger.setGlobalLogger(sys.stdout)
+if __name__ == "__main__":
 
-def check07thModServerConnection():
-	"""
-	Makes sure that we can connect to the 07th-mod server
-	(Patches will fail to download if we can't)
-	"""
-	try:
-		testFile = urlopen(Request("http://07th-mod.com/", headers={"User-Agent": ""}))
-		testFile.close()
-	except HTTPError as error:
-		print(error)
-		print("Couldn't reach 07th Mod Server.  The installer will not be able to download patch files.")
-		print("Note that we have blocked Japan from downloading (VPNs are compatible with this installer, however)")
-		common.exitWithError()
+	#redirect stdout to both a file and console
+	#TODO: on MAC using a .app file, not sure if this logfile will be writeable
+	#      could do a try-catch, and then only begin logging once the game path has been set?
+	sys.stdout = logger.Logger("logfile.log")
+	logger.setGlobalLogger(sys.stdout)
 
-check07thModServerConnection()
+	def check07thModServerConnection():
+		"""
+		Makes sure that we can connect to the 07th-mod server
+		(Patches will fail to download if we can't)
+		"""
+		try:
+			testFile = urlopen(Request("http://07th-mod.com/", headers={"User-Agent": ""}))
+			testFile.close()
+		except HTTPError as error:
+			print(error)
+			print("Couldn't reach 07th Mod Server.  The installer will not be able to download patch files.")
+			print("Note that we have blocked Japan from downloading (VPNs are compatible with this installer, however)")
+			common.exitWithError()
+
+	check07thModServerConnection()
 
 
-common.Globals.scanForExecutables()
+	common.Globals.scanForExecutables()
 
-# Scan for moddable games on the user's computer before starting installation
-modList = common.getModList("https://raw.githubusercontent.com/07th-mod/python-patcher/master/installData.json")
+	# Scan for moddable games on the user's computer before starting installation
+	modList = common.getModList("https://raw.githubusercontent.com/07th-mod/python-patcher/master/installData.json")
 
-subModconfigList = []
-for mod in modList:
-	for submod in mod['submods']:
-		conf = gameScanner.SubModConfig(mod, submod)
-		print(conf)
-		subModconfigList.append(conf)
+	subModconfigList = []
+	for mod in modList:
+		for submod in mod['submods']:
+			conf = gameScanner.SubModConfig(mod, submod)
+			print(conf)
+			subModconfigList.append(conf)
 
-gui = installerGUI.InstallerGUI(subModconfigList)
-gui.mainloop()
+	gui = installerGUI.InstallerGUI(subModconfigList)
+	gui.mainloop()
 
-exit()
+	exit()
