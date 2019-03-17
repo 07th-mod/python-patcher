728f64fcf1d5acd1485a4fd132bac6b2bf102e28
Daniel Wong <drojfjord@gmail.com>
2019-03-17 13:24:26 +1100

    Don't try to download scripts from github as they're already included in travis builds

--- bootstrap/higu_mac_installer/install.command
+++ bootstrap/higu_mac_installer/install.command
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-curl https://raw.githubusercontent.com/07th-mod/resources/master/higurashiInstaller.py | python
+cd install_data
+python ./main.py
--- bootstrap/higu_win_installer/install.bat
+++ bootstrap/higu_win_installer/install.bat
@@ -1,8 +1,6 @@
 @echo off
 cd install_data
 IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z
-aria2c --allow-overwrite=true --auto-file-renaming=false https://raw.githubusercontent.com/07th-mod/python-patcher/master/scriptDownloadList.txt
-aria2c --allow-overwrite=true --auto-file-renaming=false --input-file=scriptDownloadList.txt
 "python/python.exe" main.py
 
 echo ----------------------------------------------------------- 
