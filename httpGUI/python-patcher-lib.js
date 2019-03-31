32d83796919c1a127d9306a8ce220bb59a369c97
Daniel Wong <drojfjord@gmail.com>
2019-03-31 19:04:16 +1100

    fix navigation buttons on all pages (to call setModNameAndNavigate)

--- httpGUI/python-patcher-lib.js
+++ httpGUI/python-patcher-lib.js
@@ -6,54 +6,7 @@ let app = null;
 let el = {};
 let numberOfBlankLinesInARow = 0;
 
-// Note: { requestType, requestData } = { requestType : requestType, requestData : requestData }
-function makeJSONRequest(requestType, requestData) {
-  return JSON.stringify({ requestType, requestData });
-}
-
-function decodeJSONResponse(jsonString) {
-  const responseObject = JSON.parse(jsonString);
-  return [responseObject.responseType, responseObject.responseData];
-}
-
-// Send any object in JSON format as a POST request to the server.
-//
-// Arguments:
-//
-// - requestType (str): The type of request, as a string, sent to the server.
-//    - If incorrect, the server will send a response with type 'error'.
-//    - If correct, the server will send a response with the same type as the request
-//
-// - requestData (object): An object sent to the server with the request.
-//
-// - onSuccessCallback (function(object)): A fn executed when a response is received
-//      from the server. The fn should take the returned object as its only argument
-function doPost(requestType, requestData, onSuccessCallback) {
-  const http = new XMLHttpRequest();
-  const url = 'installer_data'; // in python, is stored in 'self.path' on the handler class
-
-  // in python, is retrieved by calling 'self.rfile.read(content_length)',
-  // where content_length is in the header (see python code)
-  http.open('POST', url, true);
-
-  // Send the proper header information along with the request
-  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-  // Call a function when the state changes.
-  // TODO: add timeout here to notify user if server has crashed or stopped working
-  http.onreadystatechange = function onReadyStateChange() {
-    if (http.readyState === 4 && http.status === 200) {
-      const [responseType, responseDataObject] = decodeJSONResponse(http.responseText);
-      if (responseType !== requestType) {
-        console.log(`ERROR: sent ${requestType} but got ${responseType}. requestData: ${responseDataObject}`);
-      } else {
-        onSuccessCallback(responseDataObject);
-      }
-    }
-  };
-
-  http.send(makeJSONRequest(requestType, requestData));
-}
+// <python-pather-rest-lib.js should be included before this file> TODO: use proper javascript import
 
 // -------------------------------- DOM Modification Functions --------------------------------
 // Adds a text node to the element with the given ID, returning the text node
