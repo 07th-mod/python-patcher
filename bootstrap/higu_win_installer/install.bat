9c0aa3b527dd4897c8758fa2badeb40bc2c8d0da
Daniel Wong <drojfjord@gmail.com>
2019-05-06 17:01:17 +1000

    Print which python is being used, and also add detection to 64-bit batch file - we might decide to go back to the 32/64 bit seperately in the future

--- bootstrap/higu_win_installer/install.bat
+++ bootstrap/higu_win_installer/install.bat
@@ -1,7 +1,14 @@
 @echo off
 cd install_data
-IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z
-"python/python.exe" main.py
+where python >nul 2>&1
+if errorlevel 1 (
+    IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z
+    echo Using bundled Python
+    "python/python.exe" main.py
+) else (
+    echo Python found on path - using system Python
+    python main.py
+)
 
 echo ----------------------------------------------------------- 
 echo ------------ Batch file has finished executing ------------
--- bootstrap/higu_win_installer_32/install.bat
+++ bootstrap/higu_win_installer_32/install.bat
@@ -3,8 +3,10 @@ cd install_data
 where python >nul 2>&1
 if errorlevel 1 (
     IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z
+    echo Using bundled Python
     "python/python.exe" main.py
 ) else (
+    echo Python found on path - using system Python
     python main.py
 )
 
