e7ff51a3750f534dfb7fbb0c1c71f19c03beb99a
Daniel Wong <drojfjord@gmail.com>
2019-05-26 12:51:27 +1000

    Try to use -E to force usage of bundled python libs instead of any already installed on the system.

--- bootstrap/higu_win_installer_32/install.bat
+++ bootstrap/higu_win_installer_32/install.bat
@@ -1,18 +1,24 @@
+setlocal
+
 @echo off
 cd install_data
 
-echo Extracting web files
-7za x -aoa httpGUI_archive.7z -ppassword
+echo Extracting files...
+IF NOT EXIST "httpGUI" 7za x -aoa httpGUI_archive.7z -ppassword
+IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z -ppassword
 
+:: Print whether the user already has python installed on path
 where python >nul 2>&1
 if errorlevel 1 (
-    IF NOT EXIST "python/python.exe" 7za x -aoa python_archive.7z -ppassword
-    echo Using bundled Python
-    "python/python.exe" main.py
+    echo INFORMATION: No Python found on path
 ) else (
-    echo Python found on path - using system Python
-    python main.py
+    echo WARNING: An existing Python was found on path
 )
+
+:: Running with -E disables checking of environment variables for python libs
+:: This should prevent conflicts with any Python dists already on the user's computer.
+echo INFORMATION: Using bundled Python to run installer
+"python/python.exe" -E main.py
 
 echo ----------------------------------------------------------- 
 echo ------------ Batch file has finished executing ------------
