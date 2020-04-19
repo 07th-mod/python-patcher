9e343c02552ea8017155f90160037bebf6bb90c6
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-04-18 21:48:00 -0500

    Download swift from official site
    Vapor's auto-install script is gone

--- travis_install_swift.sh
+++ travis_install_swift.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-eval "$(curl -sL https://raw.githubusercontent.com/vapor/vapor/master/Utilities/apt.sh)"
-sudo apt-get install swift
+sudo apt-get install clang libicu-dev
+
+SWIFT_TGZ="https://swift.org/builds/swift-5.2.2-release/ubuntu1604/swift-5.2.2-RELEASE/swift-5.2.2-RELEASE-ubuntu16.04.tar.gz"
+echo "Downloading $SWIFT_TGZ..."
+curl "$SWIFT_TGZ" | sudo tar xzf - --strip-components=1 -C /
