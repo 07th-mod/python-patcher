d448b6175c77bf016c8b3e7e09bd3ce87b17904e
drojf <drojfjord@gmail.com>
2020-06-07 20:34:29 +1000

    Fix 07th-mod check missing executable on CLI installers - Normally urlopen is used and everything works correctly, however if aria2c is used as a fallback to download, it will fail as the .exe hasn't been located yet.

--- cli_interactive.py
+++ cli_interactive.py
@@ -153,8 +153,8 @@ if __name__ == "__main__":
 	sys.stdout = logger.Logger(common.Globals.LOG_FILE_PATH)
 	logger.setGlobalLogger(sys.stdout)
 	sys.stderr = logger.StdErrRedirector(sys.stdout)
-	gui_main.check07thModServerConnection()
 	common.Globals.scanForExecutables()
+	gui_main.check07thModServerConnection()
 	modList = gui_main.getModList()
 	subModList = gui_main.getSubModConfigList(modList) #type: List[SubModConfig]
 
