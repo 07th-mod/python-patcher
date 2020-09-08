07th-mod/python-patcher

commit ca120e9453628279a8dc6fd6dec6ad5aade40505

Fix install loader cwd to always be exe location - See #110 - installer started from Start menu (by searching), will use cwd c:\Windows\system32, which is not writeable - This ensures installer always has its cwd at the location of the .exe

install_loader/src/main.rs
@@ -2,6 +2,7 @@
 
 use clap::{App, Arg, ArgMatches};
 use std::error::Error;
+use std::path::PathBuf;
 
 mod archive_extractor;
 mod config;
@@ -37,8 +38,29 @@ fn handle_open_command(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
 	}
 }
 
+fn fix_cwd() -> Result<PathBuf, Box<dyn Error>> {
+	let exe_path = std::env::current_exe()?;
+	let containing_path = exe_path.parent().ok_or("Invalid Path")?;
+	std::env::set_current_dir(containing_path)?;
+	Ok(containing_path.to_path_buf())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
 	panic_handler::set_hook(String::from("07th-mod_crash.log"));
+
+	// Change current directory to .exe path, if current .exe path is known
+	let old_cwd = std::env::current_dir();
+	match fix_cwd() {
+		Ok(new_cwd) => println!(
+			"Successfully changed path from {:?} to {:?}",
+			old_cwd, new_cwd
+		),
+		Err(e) => println!(
+			"Couldn't fix exe path - cwd remains as [{:?}]. Error: [{}]",
+			std::env::current_dir(),
+			e
+		),
+	}
 
 	let open_about_msg = r#"Shows an open dialog and:
 - if user selected a path, writes the chosen path to stdout, returns 0

commit 49b84158189385bcc17ee84bab51063c16e2432e

Fix windows file chooser (was broken in ca120e9453628279a8dc6fd6dec6ad5aade40505) - printed output would appear as file chooser parth - don't attempt to change dir if file is opened as a file chooser

install_loader/src/main.rs
@@ -50,20 +50,7 @@ fn fix_cwd() -> Result<PathBuf, Box<dyn Error>> {
 fn main() -> Result<(), Box<dyn Error>> {
 	panic_handler::set_hook(String::from("07th-mod_crash.log"));
 
-	// Change current directory to .exe path, if current .exe path is known
-	let old_cwd = std::env::current_dir();
-	match fix_cwd() {
-		Ok(new_cwd) => println!(
-			"Successfully changed path from {:?} to {:?}",
-			old_cwd, new_cwd
-		),
-		Err(e) => println!(
-			"Couldn't fix exe path - cwd remains as [{:?}]. Error: [{}]",
-			std::env::current_dir(),
-			e
-		),
-	}
-
+	//////////////////////////// Begin file chooser code ///////////////////////////////////////////
 	let open_about_msg = r#"Shows an open dialog and:
 - if user selected a path, writes the chosen path to stdout, returns 0
 - if user cancelled, writes nothing to stdout, returns 0
@@ -84,6 +71,21 @@ For example, open "text and pdf" "*.txt;*.pdf" "main c file" "main.c""#;
 
 	if let Some(matches) = matches.subcommand_matches("open") {
 		return handle_open_command(matches);
+	}
+
+	//////////////////////////// Begin normal installer code ///////////////////////////////////////
+	// Change current directory to .exe path, if current .exe path is known
+	let old_cwd = std::env::current_dir();
+	match fix_cwd() {
+		Ok(new_cwd) => println!(
+			"Successfully changed path from {:?} to {:?}",
+			old_cwd, new_cwd
+		),
+		Err(e) => println!(
+			"Couldn't fix exe path - cwd remains as [{:?}]. Error: [{}]",
+			std::env::current_dir(),
+			e
+		),
 	}
 
 	// _maybe_job must be kept in scope for the remainder of the program!