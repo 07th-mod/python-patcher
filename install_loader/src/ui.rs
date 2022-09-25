9792afd015737cbac968a048947b3b9e21e998b2
Daniel Wong <drojfjord@gmail.com>
2022-09-25 20:50:40 +1000

    Proof of concept of launching installer in webview

--- install_loader/src/ui.rs
+++ install_loader/src/ui.rs
@@ -551,6 +551,49 @@ Please download the installer to your Downloads or other known location, then ru
 		ui.text_wrapped(&self.config.sub_folder_display);
 	}
 
+	fn launch() -> wry::Result<()> {
+		use wry::{
+			application::{
+				event::{Event, StartCause, WindowEvent},
+				event_loop::{ControlFlow, EventLoop},
+				window::WindowBuilder,
+			},
+			webview::WebViewBuilder,
+		};
+
+		let event_loop = EventLoop::new();
+		let window = WindowBuilder::new()
+			.with_title("Hello World")
+			.build(&event_loop)?;
+
+		// TODO: Tell python script which port to use, OR retreive port to use from python script
+		// For now we assume python script chose port 8000, but it could choose other ports if 8000 is in use
+		let webview = WebViewBuilder::new(window)?.with_url("http://127.0.0.1:8000/")?;
+
+		#[cfg(debug_assertions)]
+		let webview = webview.with_devtools(true);
+
+		let webview = webview.build()?;
+
+		#[cfg(debug_assertions)]
+		webview.open_devtools();
+
+		// TODO: spawn event loop on new thread? Currently this freezes the launcher-ui
+		// Could also just close the launcher UI at this point as it's not really needed anymore.
+		event_loop.run(move |event, _, control_flow| {
+			*control_flow = ControlFlow::Wait;
+
+			match event {
+				Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+				Event::WindowEvent {
+					event: WindowEvent::CloseRequested,
+					..
+				} => *control_flow = ControlFlow::Exit,
+				_ => (),
+			}
+		});
+	}
+
 	// Start either the graphical or console install. Advances the installer progression to "InstallStarted"
 	fn start_install(&mut self, is_graphical: bool) -> Result<(), Box<dyn std::error::Error>> {
 		if !is_graphical {
@@ -563,6 +606,14 @@ Please download the installer to your Downloads or other known location, then ru
 			python_monitor,
 			is_graphical,
 		));
+
+		// TODO: fall back to old method of launching browser if this fails
+		match InstallerGUI::launch() {
+			Ok(_) => {}
+			Err(e) => {
+				println!("Failed to launch webview: {e}")
+			}
+		}
 
 		Ok(())
 	}
