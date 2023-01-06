07th-mod/python-patcher

commit 011eab8be68a11b9cc33a835c15c2a323e7c9174

Simplifiy format for pretty printed file sizes

common.py
@@ -774,11 +774,11 @@ def prettyPrintFileSize(fileSizeBytesWithSign):
 		sign = '-'
 
 	if fileSizeBytes >= 1e9:
-		return "{}{:.2f}".format(sign, fileSizeBytes / 1e9).strip('0').strip('.') + ' GB'
+		return "{}{:.1f}".format(sign, fileSizeBytes / 1e9).strip('0').strip('.') + ' GB'
 	elif fileSizeBytes >= 1e6:
-		return "{}{:.2f}".format(sign, fileSizeBytes / 1e6).strip('0').strip('.') + ' MB'
+		return "{}{:.0f}".format(sign, fileSizeBytes / 1e6).strip('0').strip('.') + ' MB'
 	elif fileSizeBytes > 0:
-		return "{}{:.2f}".format(sign, fileSizeBytes / 1e3).strip('0').strip('.') + ' KB'
+		return "{}{:.0f}".format(sign, fileSizeBytes / 1e3).strip('0').strip('.') + ' KB'
 	else:
 		return "0 KB"
 

commit 9b79d3a565929b9f9a1f0eb46294e93ed8b445f5

Fix pretty printed file sizes  - Previously any zeros at the end of the number would be stripped, resulting in completely wrong sizes  - Bug introduced in 011eab8be68a11b9cc33a835c15c2a323e7c9174

common.py
@@ -784,11 +784,11 @@ def prettyPrintFileSize(fileSizeBytesWithSign):
 		sign = '-'
 
 	if fileSizeBytes >= 1e9:
-		return "{}{:.1f}".format(sign, fileSizeBytes / 1e9).strip('0').strip('.') + ' GB'
+		return "{}{:.1f}".format(sign, fileSizeBytes / 1e9) + ' GB'
 	elif fileSizeBytes >= 1e6:
-		return "{}{:.0f}".format(sign, fileSizeBytes / 1e6).strip('0').strip('.') + ' MB'
+		return "{}{:.0f}".format(sign, fileSizeBytes / 1e6) + ' MB'
 	elif fileSizeBytes > 0:
-		return "{}{:.0f}".format(sign, fileSizeBytes / 1e3).strip('0').strip('.') + ' KB'
+		return "{}{:.0f}".format(sign, fileSizeBytes / 1e3) + ' KB'
 	else:
 		return "0 KB"
 