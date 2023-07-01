f8ecf91aaae485ee366474df4bcf2b594c4905de
Daniel Wong <drojfjord@gmail.com>
2023-07-01 23:21:10 +1000

    Make sure ca cert is bundled during github actions

--- travis_build_script.py
+++ travis_build_script.py
@@ -210,6 +210,9 @@ clear_folder_if_exists(staging_folder)
 # copy bootstrap folder to a temp folder
 shutil.copytree('bootstrap', bootstrap_copy_folder, dirs_exist_ok=True)
 
+# Download CA cert to be bundled with installer
+call('curl https://curl.se/ca/cacert.pem --output curl-ca-bundle.crt')
+
 # Note: previously the script created output folder in advance and then used dirs_exist_ok=True to
 # sidestep a problem in Python 3.8 where copying from the current folder
 # '.' would not ignore the destination folder even when applied as an ignore folder (the function caches
@@ -225,9 +228,6 @@ with open(os.path.join(staging_folder, build_info_path), 'w', encoding='utf-8') 
 		"build_date": f"{datetime.datetime.now()}",
 		"git_tag": f"{GIT_TAG}",
 	}, build_info_file, indent="\t", sort_keys=True)
-
-# Download CA cert to be bundled with installer
-call('curl https://curl.se/ca/cacert.pem --output curl-ca-bundle.crt')
 
 # now, copy the staged files into each os's bootstrap folder's install_data directory
 for osBootStrapPath in glob.glob(f'{bootstrap_copy_folder}/*/'):
