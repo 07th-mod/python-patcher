07th-mod/python-patcher

commit fb82b1979ad86ad9d4f958d2c1e33d854e402b41

Cache the remote version info to avoid continuously re-downloading it

fileVersionManagement.py
@@ -69,6 +69,7 @@ def parseRequirementsList(scanPath, requirementsListString):
 
 
 class VersionManager:
+	cachedRemoteVersionInfo = None
 	localVersionFileName = "installedVersionData.json"
 	def userDidPartialReinstall(self, gameInstallTimeProbePath):
 		"""
@@ -112,7 +113,12 @@ class VersionManager:
 			self.remoteVersionInfo = _testRemoteSubModVersion
 		else:
 			try:
-				self.remoteVersionInfo = getRemoteVersion(self.targetID)
+				# Cache the remote version info to avoid continuously re-downloading it
+				if VersionManager.cachedRemoteVersionInfo is None:
+					self.remoteVersionInfo = getRemoteVersion(self.targetID)
+					VersionManager.cachedRemoteVersionInfo = self.remoteVersionInfo
+				else:
+					self.remoteVersionInfo = VersionManager.cachedRemoteVersionInfo
 			except Exception as error:
 				self.remoteVersionInfo = None
 				print("VersionManager: Error while retrieving remote version information {}".format(error))

commit a35549dc267e9d43384f1742d7ca05eafb6491f1

Fix #217 - installer trying to install wrong mod when switching mods  - Broken by fb82b1979ad86ad9d4f958d2c1e33d854e402b41 as I did not include the self.targetID as part of the caching key

fileVersionManagement.py
@@ -69,7 +69,7 @@ def parseRequirementsList(scanPath, requirementsListString):
 
 
 class VersionManager:
-	cachedRemoteVersionInfo = None
+	cachedRemoteVersionInfo = {}
 	localVersionFileName = "installedVersionData.json"
 	def userDidPartialReinstall(self, gameInstallTimeProbePath):
 		"""
@@ -114,11 +114,10 @@ class VersionManager:
 		else:
 			try:
 				# Cache the remote version info to avoid continuously re-downloading it
-				if VersionManager.cachedRemoteVersionInfo is None:
+				self.remoteVersionInfo = VersionManager.cachedRemoteVersionInfo.get(self.targetID)
+				if self.remoteVersionInfo is None:
 					self.remoteVersionInfo = getRemoteVersion(self.targetID)
-					VersionManager.cachedRemoteVersionInfo = self.remoteVersionInfo
-				else:
-					self.remoteVersionInfo = VersionManager.cachedRemoteVersionInfo
+					VersionManager.cachedRemoteVersionInfo[self.targetID] = self.remoteVersionInfo
 			except Exception as error:
 				self.remoteVersionInfo = None
 				print("VersionManager: Error while retrieving remote version information {}".format(error))